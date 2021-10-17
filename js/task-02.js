faa897a32d7ff15eeb7c57f9606b9bb35766efd2
Roman Dachko <roman8301@gmail.com>
2021-10-17 18:25:16 +0300

    Task-02 completed

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const allList = document.querySelector('#ingredients');
+
+const listEl = ingredients.map(ingredient => {
+  const element = document.createElement('li');
+  element.textContent = ingredient;
+  element.classList.add('item');
+
+  return element;
+});
+
+allList.append(...listEl);
