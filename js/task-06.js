fddca088e0c1ca506d622b7e43793474e6c73067
Roman Dachko <roman8301@gmail.com>
2021-10-26 17:59:08 +0300

    Task 6 completed

--- js/task-06.js
+++ js/task-06.js
@@ -1,25 +1,12 @@
 'use strict';
 
-//validation - input;
+const input = document.querySelector('input#validation-input');
 
-const input = document.querySelector('#validation-input');
-const inputDataLength = input.getAttribute('data-length');
-
-function onKeypressInput(valid) {
-  if (input.textContent.length === inputDataLength) {
-    return input.classList.add('#validation-input.valid');
+const validationInput = input.addEventListener('blur', event => {
+  if (input.value.length === Number(input.dataset.length)) {
+    input.classList.add('valid');
+  } else {
+    input.classList.remove('valid');
+    input.classList.add('invalid');
   }
-  return input.classList.add('#validation-input.invalid');
-}
-
-inputRef.addEventListener('keydown', onKeypressInput);
-
-// function onKeypress(event) {
-//   console.log(event.key);
-// }
-
-// function validationInput(event) {
-//   console.log(inputRef);
-// }
-
-// inputRef.addEventListener('click', validationInput);
+});
