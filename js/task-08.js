7f2671175513d200264bcee204f35ad3a506d2d2
Roman Dachko <roman8301@gmail.com>
2021-10-28 22:38:36 +0300

    Task 08 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+'use strict';
 
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', onSubmittingAForm);
+
+function onSubmittingAForm(evt) {
+  evt.preventDefault();
+  if (
+    evt.currentTarget.elements.password.value === '' ||
+    evt.currentTarget.elements.email.value === ''
+  ) {
+    alert('All form fields must be completed!');
+  } else {
+    const dataLoginForm = {
+      email: evt.currentTarget.elements.email.value,
+      password: evt.currentTarget.elements.password.value,
+    };
+    console.log(dataLoginForm);
+    evt.currentTarget.reset();
+  }
+}
