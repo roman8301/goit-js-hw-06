198f095ff2923b7e08b0017cbbe48a01cd535a0d
Roman Dachko <roman8301@gmail.com>
2021-10-30 12:11:30 +0300

    Task-09 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+'use strict';
+
+const colorValue = document.querySelector('.color');
+const buttonChangeColor = document.querySelector('.change-color');
+
+buttonChangeColor.addEventListener('click', onButtonClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onButtonClick(event) {
+  colorValue.textContent = getRandomHexColor();
+
+  if (event.target.classList.contains('change-color')) {
+    return (document.body.style.background = getRandomHexColor());
+  }
+  return;
 }
