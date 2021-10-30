a8ca33105d01e6e77ecc9c1091de35b5317b73ba
Roman Dachko <roman8301@gmail.com>
2021-10-30 15:36:44 +0300

    Task 10 completed

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('input');
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const container = document.querySelector('#boxes');
+
+createButton.addEventListener('click', getInputValue);
+destroyButton.addEventListener('click', destroyBoxes);
+
+function getInputValue() {
+  createBoxes(input.value);
+}
+
+function createBoxes(amount) {
+  let size = 30;
+  const divElements = [];
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    div.style.width = size + 'px';
+    div.style.height = size + 'px';
+    div.style.backgroundColor = getRandomHexColor();
+    divElements.push(div);
+    size += 10;
+  }
+  container.append(...divElements);
+}
+
+function destroyBoxes() {
+  container.innerHTML = '';
+}
