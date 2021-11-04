b936c7da6fc6d4ce6ed5143cb758f0356c695e60
Roman Dachko <roman8301@gmail.com>
2021-11-04 11:57:57 +0200

    Property changed 'textEl.style.fontSize' in task-07

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,7 @@
 
 const fontSizeControl = document.getElementById('font-size-control');
 const textEl = document.getElementById('text');
-textEl.style.fontSize = '56px';
+textEl.style.fontSize = fontSizeControl.value + 'px';
 
 fontSizeControl.addEventListener('input', () => {
   textEl.style.fontSize = fontSizeControl.value + 'px';
